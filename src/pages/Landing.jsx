4b911df9a4de114abb9ce64f89f3652524a5825b
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-05-08 16:39:24 -0500

    structure/clean-structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,9 @@
-import { Button, Typography, Box, useTheme } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Box, useTheme } from "@mui/material";
+
 
 export default function Landing() {
     const theme = useTheme();
-    const navigate = useNavigate();
-
-    const handleSignIn = () => {
-        navigate("/signin");
-    };
-
-    const handleSignUp = () => {
-        navigate("/signup");
-    };
-
+    
     return (
         <>
             <Box
@@ -26,68 +17,11 @@ export default function Landing() {
                     backgroundColor: theme.palette.background.gray,
                     backgroundSize: 'cover',
                     minHeight: '100vh',
-                }}
-            >
-                <img
-                    src="/public/shadowHead.png"
-                    alt="shadow"
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                        zIndex: 1,
-                        maxWidth: '50%',
-                    }}
-                />
-                <img
-                    src="/public/logo.png"
-                    alt="wisehodler logo"
-                    className="absolute top-0 right-0"
-                    style={{ zIndex: 2, maxWidth: '100%' }}
-                />
-                <div style={{ marginTop: theme.spacing(5), width: '100%', textAlign: 'center' }}>
-                    <div style={{ marginBottom: theme.spacing(2) }}>
-                        <Button
-                            sx={{
-                                backgroundColor: theme.palette.primary2.main,
-                                width: "200px",
-                                height: "40px",
-                                color: theme.palette.primary2.contrastText,
-                            }}
-                            onClick={handleSignIn}>
-                            Sign In
-                        </Button>
-                    </div>
-                    <div>
-                        <Button
-                            sx={{
-                                backgroundColor: theme.palette.primary2.main,
-                                width: "200px",
-                                height: "40px",
-                                color: theme.palette.primary2.contrastText,
-                            }}
-                            onClick={handleSignUp}>
-                            Sign Up
-                        </Button>
-                    </div>
-                </div>
-                <Typography variant="body1" style={{
-                    color: theme.palette.primary2.contrastText,
-                    marginTop: theme.spacing(5), fontSize: '18px'
-                }}>
-                    Next Level Crypto Wallet
-                </Typography>
-                <img
-                    src="/public/shadowBottom.png"
-                    alt="shadow"
-                    style={{
-                        position: 'absolute',
-                        bottom: 0,
-                        left: 0,
-                        zIndex: 1,
-                        maxWidth: '50%',
-                    }}
-                />
+                }}           >
+
+                <h1>ESTA ES MI VISTA LANDING</h1>               
+            
+               
             </Box>
         </>
     );
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,9 +1,6 @@
 import { useRoutes } from "react-router-dom"
 import Landing from "../pages/Landing"
-import SignIn from "../pages/SignIn"
-import SignUp from "../pages/SignUp"
-import WalletSelect from "../pages/WalletSelect"
-import UserMenu from "../pages/UserMenu"
+
 
 
 
@@ -13,23 +10,7 @@ function Router() {
       path: "/",
       element: <Landing />
     },
-    {
-      path: "/signin",
-      element: <SignIn />
-    },
-    {
-      path: "/signup",
-      element: <SignUp />
-    },
-    {
-      path: "/wallets",
-      element: <WalletSelect />
-    },
-    {
-      path: "/menu",
-      element: <UserMenu />
-    },
-
+    
   ])
 }
 
