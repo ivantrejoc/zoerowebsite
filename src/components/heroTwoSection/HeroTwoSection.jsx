e9a101f632cff5434aa5627c27037590dc3f259f
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-05-14 10:34:15 -0500

    hero-2-section/atomization

---
+++ src/components/heroTwoSection/HeroTwoSection.jsx
@@ -0,0 +1,79 @@
+import { Box, Typography, Avatar } from "@mui/material";
+import zakariaAvatar from "../../assets/img/zakaria-avatar.png";
+const HeroTwoSection = () => {
+  return (
+    <Box
+        id="hero-2"
+        sx={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "start",
+          position: "relative",
+          width: "100vw",
+          height: "50vh",
+          maxHeight: "50vh",
+          paddingLeft: "8.25vw",
+          marginBottom: "15vh"
+        }}
+      >
+        <Box
+          id="hero-contanier"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            alignItems: "start",
+            position: "relative",
+            gap: 2.19,
+            width: "70.25rem",
+            height: "100%"
+          }}
+        >
+          {" "}
+          <div id="text-container">
+            <div>
+              <p id="hero-2-text">
+                {
+                  '"In math, we find certainty among variables. Zoero applies this principle to navigate complex business landscapes."'
+                }
+              </p>
+            </div>
+          </div>
+          <div id="avatar-container">
+            <div id="photo-container">
+              <Avatar
+                alt="zakaria-ambari"
+                src={zakariaAvatar}
+                variant="rounded"
+                sx={{
+                  width: "4.375rem",
+                  height: "4.375rem",
+                  flexShrink: "0"
+                }}
+              />
+            </div>
+            <div>
+              <p id="name">Zakaria Ambari</p>
+              <Typography
+                variant="workPosition"
+                component="p"
+                sx={{ margin: 0 }}
+              >
+                Zœro Mathematician{" "}
+              </Typography>
+            </div>
+          </div>
+        </Box>
+        {/* <Divider variant="fullWidth" orientation="horizontal"
+        absolute sx={{
+          display: "flex",
+          color: "red",
+         width: 3,
+          height: "2rem",
+          flexShrink: 0,
+          marginTop: "10rem"
+        }} /> */}
+      </Box>
+  )
+}
+export default HeroTwoSection
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import "../assets/css/_default.css";
-import { Box, useTheme, Avatar, Typography } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import mainBackground from "../assets/img/vector-103-stroke.png";
 import Header from "../components/header/Header.jsx";
 import HeroSection from "../components/heroSection/HeroSection.jsx";
@@ -9,7 +9,8 @@ import ExperimentsSection from "../components/experimentsSection/ExperimentsSect
 import binaryStripe from "../assets/img/binary-stripe.png";
 import CoefficientsSection from "../components/coefficientsSection/CoefficientsSection.jsx";
 import FieldsMedalsSection from "../components/fieldsMedalsSection/FieldsMedalsSection.jsx";
-import zakariaAvatar from "../assets/img/zakaria-avatar.png";
+import HeroTwoSection from "../components/heroTwoSection/HeroTwoSection.jsx";
+
 
 export default function Landing() {
   const theme = useTheme();
@@ -75,78 +76,7 @@ export default function Landing() {
       <ButtonArea />
 
       {/* Hero-2 */}
-      <Box
-        id="hero-2"
-        sx={{
-          display: "flex",
-          justifyContent: "start",
-          alignItems: "start",
-          position: "relative",
-          width: "100vw",
-          height: "50vh",
-          maxHeight: "50vh",
-          paddingLeft: "8.25vw",
-          marginBottom: "15vh"
-        }}
-      >
-        <Box
-          id="hero-contanier"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "start",
-            alignItems: "start",
-            position: "relative",
-            gap: 2.19,
-            width: "70.25rem",
-            height: "100%"
-          }}
-        >
-          {" "}
-          <div id="text-container">
-            <div>
-              <p id="hero-2-text">
-                {
-                  '"In math, we find certainty among variables. Zoero applies this principle to navigate complex business landscapes."'
-                }
-              </p>
-            </div>
-          </div>
-          <div id="avatar-container">
-            <div id="photo-container">
-              <Avatar
-                alt="zakaria-ambari"
-                src={zakariaAvatar}
-                variant="rounded"
-                sx={{
-                  width: "4.375rem",
-                  height: "4.375rem",
-                  flexShrink: "0"
-                }}
-              />
-            </div>
-            <div>
-              <p id="name">Zakaria Ambari</p>
-              <Typography
-                variant="workPosition"
-                component="p"
-                sx={{ margin: 0 }}
-              >
-                Zœro Mathematician{" "}
-              </Typography>
-            </div>
-          </div>
-        </Box>
-        {/* <Divider variant="fullWidth" orientation="horizontal"
-        absolute sx={{
-          display: "flex",
-          color: "red",
-         width: 3,
-          height: "2rem",
-          flexShrink: 0,
-          marginTop: "10rem"
-        }} /> */}
-      </Box>
+      <HeroTwoSection />      
     </Box>
   );
 }
