77311a55f4048584e9763a1d9ac71253dc5e9515
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-05-27 08:56:52 -0500

    footer/atomization

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,70 @@
+import { Box } from "@mui/material";
+import zoeroLogo from "../../assets/img/zoero-logo.svg";
+const Footer = () => {
+  return (
+    <Box
+      id="footer-container"
+      sx={{
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+        position: "relative",
+        width: "100vw",
+        height: "40vh",
+        marginBottom: "0"
+      }}
+    >
+      <Box
+        id="footer-items-container"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          position: "relative",
+          width: "84vw",
+          height: "100%",
+          margin: "0 8vw 0 8vw"
+        }}
+      >
+        <div id="left-items">
+          <div id="logo-container">
+            <img id="logo-footer" src={zoeroLogo} alt="zoero-logo" />
+          </div>
+          <p className="rights">
+            © Zoero Labs LTD. <br></br>
+            All Rights Reserved 2024.
+          </p>
+          <p id="solvedText">
+            Solved by <span className="highlight">@Omar</span> and{" "}
+            <span className="highlight">@Zakaria</span>
+          </p>
+        </div>
+        <div id="right-items">
+          <div id="nav-1">
+            <a className="footerLinks" href="#">
+              The Science
+            </a>
+            <a className="footerLinks" href="#">
+              Millenniums
+            </a>
+            <a className="footerLinks" href="#">
+              Coefficients
+            </a>
+          </div>
+          <div id="nav-2">
+            <a className="footerLinks" href="#">
+              Knowledge
+            </a>
+            <a className="footerLinks" href="#">
+              Terms & Policies
+            </a>
+            <a className="footerLinks" href="#">
+              Contact us
+            </a>
+          </div>
+        </div>
+      </Box>
+    </Box>
+  );
+};
+export default Footer;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,12 +6,11 @@ import HeroSection from "../components/heroSection/HeroSection.jsx";
 import ReasoningSection from "../components/reasoningSection/ReasoningSection.jsx";
 import ButtonArea from "../components/buttonArea/ButtonArea.jsx";
 import ExperimentsSection from "../components/experimentsSection/ExperimentsSection.jsx";
+import Footer from "../components/footer/Footer.jsx";
 import binaryStripe from "../assets/img/binary-stripe.png";
 import CoefficientsSection from "../components/coefficientsSection/CoefficientsSection.jsx";
 import FieldsMedalsSection from "../components/fieldsMedalsSection/FieldsMedalsSection.jsx";
 import HeroTwoSection from "../components/heroTwoSection/HeroTwoSection.jsx";
-
-import zoeroLogo from "../assets/img/zoero-logo.svg";
 
 export default function Landing() {
   const theme = useTheme();
@@ -75,63 +74,12 @@ export default function Landing() {
       <FieldsMedalsSection />
       {/* Button Area 2 */}
       <ButtonArea />
-
       {/* Hero-2 */}
       <HeroTwoSection />
-      
-{/* FALTA INSERTAR DIVIDER QUE NO LO ESTÁ RENDERIZANDO */}
+
+      {/* FALTA INSERTAR DIVIDER QUE NO LO ESTÁ RENDERIZANDO */}
       {/* Footer     */}
-      <Box
-        id="footer-container"
-        sx={{
-          display: "flex",
-          justifyContent: "start",
-          alignItems: "center",
-          position: "relative",         
-          width: "100vw",
-          height: "40vh",
-          marginBottom: "0"
-        }}
-      >
-  <Box
-        id="footer-items-container"
-        sx={{
-          display: "flex",          
-          alignItems: "center",
-          justifyContent: "space-between",
-          position: "relative",          
-          width: "84vw",
-          height: "100%",
-          margin: "0 8vw 0 8vw"
-        
-        }}
-      >    
-        <div id="left-items">
-          <div id="logo-container">
-            <img id="logo-footer" src={zoeroLogo} alt="zoero-logo" />
-          </div>
-          <p className="rights">© Zoero Labs LTD. <br></br>
-          All Rights Reserved 2024.
-</p>
-          <p id="solvedText">Solved by <span className="highlight">@Omar</span> and <span className="highlight">@Zakaria</span></p>
-        </div>
-        <div id="right-items">
-          <div id="nav-1">
-            <a className="footerLinks" href="#">The Science</a>
-            <a className="footerLinks" href="#">Millenniums</a>
-            <a className="footerLinks" href="#">Coefficients</a>
-          </div>
-          <div id="nav-2">
-            <a className="footerLinks" href="#">Knowledge</a>
-            <a className="footerLinks"href="#">Terms & Policies</a>
-            <a className="footerLinks" href="#">Contact us</a>
-          </div>          
-        </div>
-
-      </Box>
-
-      </Box>
-
+      <Footer />
     </Box>
   );
 }
