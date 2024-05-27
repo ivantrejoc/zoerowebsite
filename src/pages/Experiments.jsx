4782d9e502830a66f7a257ee0f9bfc79c5d934c5
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-05-27 16:39:45 -0500

    main-hero/layout

--- src/pages/Experiments.jsx
+++ src/pages/Experiments.jsx
@@ -17,7 +17,65 @@ const Experiments = () => {
         backgroundSize: "cover"
       }}
     >
-        EXPERIMENTS
+      <Box
+        id="hero-experiment-container"
+        sx={{
+          background: "plum",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          flexDirection: "column",
+          position: "relative",
+          width: "100%",
+          overflowX: "0px",
+          height: "45vh",
+          maxHeight: "45vh",
+          marginBottom: 12
+        }}
+      >
+        <Box
+          sx={{
+            background: "gold",
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "start",
+            flexDirection: "column",
+            position: "relative",
+            width: "87%",
+            height: "100%",
+            margin: "0 6.5vw 0 6.5vw",
+            maxHeight: "100%"
+          }}
+        >
+          <Box
+            sx={{
+              background: "red",
+              width: "25%",
+              height: "33%",
+              display: "flex",
+              flexDirection: "column"
+            }}
+          ></Box>
+          <Box
+            sx={{
+              background: "green",
+              width: "80%",
+              height: "33%",
+              display: "flex",
+              flexDirection: "column"
+            }}
+          ></Box>
+          <Box
+            sx={{
+              background: "blue",
+              width: "40%",
+              height: "33%",
+              display: "flex",
+              flexDirection: "column"
+            }}
+          ></Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
