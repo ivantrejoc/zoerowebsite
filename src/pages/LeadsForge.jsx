c06ab6ae169d1d41475011c149ff573d2e4a4547
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-05-28 11:02:03 -0500

    leads-forge/main-hero

---
+++ src/pages/LeadsForge.jsx
@@ -0,0 +1,158 @@
+import { Box, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import xicon from "../assets/icons/x-icon.svg";
+
+
+
+const LeadsForge = () => {
+    const theme = useTheme();
+  return (
+    <Box
+      id="leads-forge-main-body"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "stretch",
+        flexDirection: "column",
+        position: "relative",
+        backgroundColor: theme.palette.background.default,
+        backgroundSize: "cover"
+      }}
+    >
+      <Box
+        id="hero-leadsForge-container"
+        sx={{
+          background: "plum",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          flexDirection: "column",
+          position: "relative",
+          width: "100%",
+          overflowX: "0px",
+          height: "30vh",
+          maxHeight: "30vh",
+          marginBottom: 12
+        }}
+      >
+        <Box
+          sx={{
+            background: "gold",
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "start",
+            flexDirection: "column",
+            position: "relative",
+            width: "86%",
+            height: "100%",
+            margin: "0 7vw 0 7vw",
+            maxHeight: "100%"
+          }}
+        >
+          <Box
+            sx={{
+              width: "15%",
+              height: "9vh",
+              display: "flex",
+              gap: 1,
+              marginBottom: 2
+            }}
+          >
+            <Typography
+              variant="h2"
+              sx={{
+                fontFamily: "Urbanist",
+                fontSize: "5rem",
+                fontStyle: "normal",
+                fontWeight: "700",
+                lineHeight: "100%"
+              }}
+              component="h2"
+            >
+              {/* EXE */}
+            </Typography>
+            <Typography
+              variant="text"
+              component="p"
+              sx={{
+                width: "10%",
+                paddingTop: 2
+              }}
+            >
+              {/* Case Study */}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "60%",
+              height: "12vh",
+              display: "flex",
+              flexDirection: "column",
+              marginBottom: 2
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "Urbanist",
+                fontSize: "2.5rem",
+                fontStyle: "normal",
+                fontWeight: "600",
+                lineHeight: "112%"
+              }}
+              component="p"
+            >
+              {/* An innovative workout tracker app designed to optimize your
+              exercise regimen and accuracy. */}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "30%",
+              height: "5vh",
+              display: "flex",
+              gap: 6
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2
+              }}
+            >
+              <img id="x-icon" src={xicon} alt="x-icon" />{" "}
+              <Typography
+                variant="text"
+                sx={{
+                  justifyItems: "center",
+                  alignContent: "center"
+                }}
+                component="p"
+              >
+                Brand Design
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2
+              }}
+            >
+              <img id="x-icon" src={xicon} alt="x-icon" />{" "}
+              <Typography
+                variant="text"
+                sx={{
+                  justifyItems: "center",
+                  alignContent: "center"
+                }}
+                component="p"
+              >
+                Web Crafting
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+export default LeadsForge
