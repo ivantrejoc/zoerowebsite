fa292b51880940dde6b365f19cf55c1a7e116a15
miguelmontoya <miguel.angel@conexioncibernetica.com>
2024-06-13 16:07:57 -0500

    title animated

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useRef } from 'react';
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { gsap } from 'gsap';
 
 const HeroSection = () => {
   const theme = useTheme();
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    // GSAP animation for the title
+    gsap.fromTo(
+      titleRef.current,
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, ease: "power1.out", delay: 0.5 }
+    );
+  }, []);
 
   return (
     <Box
@@ -18,11 +30,12 @@ const HeroSection = () => {
         maxHeight: "120vh",
         [theme.breakpoints.down("xxl")]: {
           height: "120vh",
-          justifyContent: "center"
-        }
+          justifyContent: "center",
+        },
       }}
     >
       <Typography
+        ref={titleRef}
         variant="h1"
         component="h1"
         sx={{
@@ -30,30 +43,29 @@ const HeroSection = () => {
           marginTop: "2vh",
           whiteSpace: "pre-line",
           [theme.breakpoints.down("xxxl")]: {
-            fontSize: "10.275rem"
+            fontSize: "10.275rem",
           },
-
           [theme.breakpoints.down("xxl")]: {
-            fontSize: "9rem"
+            fontSize: "9rem",
           },
           [theme.breakpoints.down("xl")]: {
-            fontSize: "6rem"
+            fontSize: "6rem",
           },
           [theme.breakpoints.down("lg")]: {
-            fontSize: "5.3375rem"
+            fontSize: "5.3375rem",
           },
           [theme.breakpoints.down("md")]: {
-            fontSize: "5.4375rem"
+            fontSize: "5.4375rem",
           },
           [theme.breakpoints.down("sm")]: {
-            fontSize: "3.3375rem"
+            fontSize: "3.3375rem",
           },
           [theme.breakpoints.down("xs")]: {
-            fontSize: "2.6375rem"
+            fontSize: "2.6375rem",
           },
           [theme.breakpoints.down("xxs")]: {
-            fontSize: "2.2375rem"
-          }
+            fontSize: "2.2375rem",
+          },
         }}
       >
         &nbsp;WE SOLVE THE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;♦&nbsp;MILLENNIUM
@@ -62,4 +74,5 @@ const HeroSection = () => {
     </Box>
   );
 };
+
 export default HeroSection;
